de6307028b76938c5a0ff4cd39475f7a5f3e3329
Ryo <19246551+sakoyan@users.noreply.github.com>
2018-10-05 17:34:00 +0900

    Update ros_kinettic_mapping.sh

--- ros_kinettic_mapping.sh
+++ ros_kinettic_mapping.sh
@@ -1,1 +1,1 @@
-sudo apt install -y ros-kinetic-gmapping ros-kinetic-geometry* ros-kinetic-openslam-gmapping ros-kinetic-slam* ros-kinetic-map*
+sudo apt install -y ros-kinetic-gmapping ros-kinetic-geometry* ros-kinetic-openslam-gmapping ros-kinetic-slam* ros-kinetic-map* ros-kinetic-rosserial*
